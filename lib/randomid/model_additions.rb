99a0882a640afa436cb88f0043a6b72420fff039
Harry <harrisonlucas@Live.com>
2016-01-02 21:18:58 +1100

    Tidied up a bit

--- lib/randomid/model_additions.rb
+++ lib/randomid/model_additions.rb
@@ -1,8 +1,8 @@
 module Randomid
   module ModelAdditions
-
+    # TODO: Refactor
     def unique_identifier(attribute, length=16)
-      before_create do 
+      before_create do
         loop do
           token = Randomid.generate_uid(length)
           break send("#{attribute}=", token) unless self.class.exists?("#{attribute}" => token)
